1bda933fd62cd214d06e9c26f34edfcfcb97b2dc
Josh Mu <mu@joshmu.com>
2016-03-10 15:47:41 +1100

    added promise library

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-//TODO: use q library to fulfill all promises before sending
 //TODO: index page warns this will take a bit of time to propogate results
 
 var express = require('express');
 var app = express();
+
 var xray = require('x-ray');
 var x = new xray();
+
+var Q = require('q');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,36 +18,67 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+// SCRAPE ALL SITES AVAILABLE
 function scrape(response) {
-  // AUSTRALIA COUNCIL
-  x('http://www.australiacouncil.gov.au/funding/new-grants-model/', 'div.faqs li', [{
+  Q.all([
+    australiaCouncil(),
+    regionalArtsNsw()
+  ]).done(function(data) {
+    response.send(data);
+  }, console.error);
+}
+
+////////////////////////////////////////////////////////////////
+//      WEBSITES
+///////////////////////////////////////////////////////////////
+
+// AUSTRALIA COUNCIL
+function australiaCouncil() {
+  var url = 'http://www.australiacouncil.gov.au/funding/new-grants-model/';
+  var deferred = Q.defer();
+  x(url, 'div.faqs li', [{
     grant: 'a',
     dates: x('ul', ['li']),
     info: x('div', ['p']),
     link: 'a.btn@href'
   }])(function(error, data) {
+    if (error) deferred.reject(error);
     // if there is no info present it most likely won't be a valid entry
     var data = data.filter(function(item) {
       return item.info.length > 0;
     });
     // join all the p tags together
-    data.forEach(function(item) {
-      item.info = item.info.join('  ');
+    data.forEach(function(grant) {
+      grant.info = grant.info.join('  ');
     });
-    next(response, data);
+    var output = {
+      title: url,
+      data: data
+    };
+    deferred.resolve(output);
   });
-
+  return deferred.promise;
 }
 
-function next(response, DATA) {
-  x('http://regionalartsnsw.com.au/grants/?grant_category=dance', 'div.grant', [{
+
+// REGIONAL ARTS NSW
+function regionalArtsNsw() {
+  var url = 'http://regionalartsnsw.com.au/grants/?grant_category=dance';
+  var deferred = Q.defer();
+  x(url, 'div.grant', [{
     grant: '.grant_title',
     info: ['p'],
     link: 'a.external@href'
   }])(function(err, data) {
-          data.forEach(function(entry) {
-              entry.info = entry.info.join(' ');
-          });
-        response.send(DATA.concat(data));
+    if (err) deferred.reject(err);
+    data.forEach(function(grant) {
+      grant.info = grant.info.join(' ');
+    });
+    var output = {
+      title: url,
+      data: data
+    };
+    deferred.resolve(output);
   });
+  return deferred.promise;
 }
--- x.js
+++ x.js
@@ -1,36 +1,62 @@
 var Xray = require('x-ray');
 var x = new Xray();
+var Q = require('q');
+
+// SCRAPE ALL SITES AVAILABLE
+Q.all([
+  australiaCouncil(),
+  regionalArtsNsw()
+]).done(function(data) {
+  console.log(data);
+}, console.error);
 
 // AUSTRALIA COUNCIL
-x('http://www.australiacouncil.gov.au/funding/new-grants-model/', 'div.faqs li', [{
+function australiaCouncil() {
+  var url = 'http://www.australiacouncil.gov.au/funding/new-grants-model/';
+  var deferred = Q.defer();
+  x(url, 'div.faqs li', [{
     grant: 'a',
     dates: x('ul', ['li']),
     info: x('div', ['p']),
     link: 'a.btn@href'
   }])(function(error, data) {
-      // if there is no info present it most likely won't be a valid entry
-      var data = data.filter(function(item) {
-        return item.info.length > 0;
-      });
-      // join all the p tags together
-      data.forEach(function(item) {
-        item.info = item.info.join('  ');
-      });
-      next(data);
+    if (error) deferred.reject(error);
+    // if there is no info present it most likely won't be a valid entry
+    var data = data.filter(function(item) {
+      return item.info.length > 0;
     });
-
-function next(DATA) {
-
-  // REGIONAL ARTS NSW
-    x('http://regionalartsnsw.com.au/grants/?grant_category=dance', 'div.grant', [{
-      grant: '.grant_title',
-      info: ['p'],
-      link: 'a.external@href'
-    }])(function(err, data) {
-            data.forEach(function(entry) {
-                entry.info = entry.info.join(' ');
-            });
-        console.log(DATA.concat(data));
+    // join all the p tags together
+    data.forEach(function(grant) {
+      grant.info = grant.info.join('  ');
     });
+    var output = {
+      title: url,
+      data: data
+    };
+    deferred.resolve(output);
+  });
+  return deferred.promise;
+}
 
+
+// REGIONAL ARTS NSW
+function regionalArtsNsw() {
+  var url = 'http://regionalartsnsw.com.au/grants/?grant_category=dance';
+  var deferred = Q.defer();
+  x(url, 'div.grant', [{
+    grant: '.grant_title',
+    info: ['p'],
+    link: 'a.external@href'
+  }])(function(err, data) {
+    if (err) deferred.reject(err);
+    data.forEach(function(grant) {
+      grant.info = grant.info.join(' ');
+    });
+    var output = {
+      title: url,
+      data: data
+    };
+    deferred.resolve(output);
+  });
+  return deferred.promise;
 }
